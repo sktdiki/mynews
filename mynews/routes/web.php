9872a86d7bce2a231903ebb2213171255b640e25
sakita <sktdik.18@gmail.com>
2021-10-20 19:41:24 +0000

    課題4

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -14,14 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-Route::group( ['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-});
-Route::get('XXX', 'AAAController@bbb');
-
-
-    Route::get('admin/profile/create', 'Admin\ProfileController@add');
-    Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-eqiv="X-UA-Compatible"content="IE=edge">
+        <meta name="vieport" content="width=dwidth,initial-scale=1">
+        
+        <title>profile</title>
+    </head>
+    <body>
+        <h1>edit Action</h1>
+    </body>
+
+</html>
